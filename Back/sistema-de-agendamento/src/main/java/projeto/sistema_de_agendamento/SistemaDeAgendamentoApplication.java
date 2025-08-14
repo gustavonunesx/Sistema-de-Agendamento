b0df045bd20465ef4d5ae6263768d98a814c04a4
= <=>
2025-08-14 16:34:33 -0300

    structure

---
+++ Back/sistema-de-agendamento/src/main/java/projeto/sistema_de_agendamento/SistemaDeAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package projeto.sistema_de_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAgendamentoApplication.class, args);
+	}
+
+}
---
+++ Back/sistema-de-agendamento/src/test/java/projeto/sistema_de_agendamento/SistemaDeAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.sistema_de_agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
